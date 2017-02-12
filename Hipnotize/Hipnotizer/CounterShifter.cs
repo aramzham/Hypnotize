a1d7a4f5ca7f154df26028fad6d516595f38feb3
aramzham <aramzham@hotmail.com>
2017-02-12 15:05:41 +0400

    illusion

---
+++ Hipnotize/Hipnotizer/CounterShifter.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Hipnotizer
+{
+    public class CounterShifter
+    {
+        public int[][] CreateMatrix(int n)
+        {
+            var r = new Random((int) DateTime.Now.Ticks);
+            
+        }
+
+        public static int[][] Start(int[][] matrix)
+        {
+            var contours = new List<List<int>>();
+            var minLength = Math.Min(matrix.Length, matrix[0].Length);
+            var contourCount = minLength / 2 + minLength % 2;
+            var result = new int[matrix.Length][];
+            for (int m = 0; m < result.Length; m++) //initializing resulting array
+                result[m] = new int[matrix[m].Length];
+            var assign = false;
+            var i = 0;
+            var j = 0;
+            var l = 0;
+            for (int k = 0; k < contourCount; k++)
+            {
+                i = j = k;
+                if (!assign) contours.Add(new List<int>());
+                while (j < matrix[i].Length - k)
+                {
+                    if (!assign) contours[k].Add(matrix[i][j]);
+                    else
+                    {
+                        result[i][j] = contours[k][l];
+                        l++;
+                    }
+                    j++;
+                }
+                j--;
+                i++;
+                if (i == matrix.Length - k) j = k - 1;
+                while (i < matrix.Length - k)
+                {
+                    if (!assign) contours[k].Add(matrix[i][j]);
+                    else
+                    {
+                        result[i][j] = contours[k][l];
+                        l++;
+                    }
+                    i++;
+                }
+                i--;
+                j--;
+                while (j >= 0 + k)
+                {
+                    if (!assign) contours[k].Add(matrix[i][j]);
+                    else
+                    {
+                        result[i][j] = contours[k][l];
+                        l++;
+                    }
+                    j--;
+                }
+                j++;
+                i--;
+                if (k == matrix[i].Length / 2) i = k;
+                while (i > 0 + k)
+                {
+                    if (!assign) contours[k].Add(matrix[i][j]);
+                    else
+                    {
+                        result[i][j] = contours[k][l];
+                        l++;
+                    }
+                    i--;
+                }
+                l = 0;
+                if (!assign)
+                {
+                    if (k % 2 == 0) contours[k] = RightShift(contours[k]);
+                    else contours[k] = LeftShift(contours[k]);
+                    if (k == contourCount - 1)
+                    {
+                        k = -1;
+                        assign = true;
+                    }
+                }
+            }
+            return result;
+        }
+        private List<int> LeftShift(List<int> list)
+        {
+            return list.Skip(1).Concat(list.Take(1)).ToList();
+        }
+        private List<int> RightShift(List<int> list)
+        {
+            return list.Skip(list.Count - 1).Concat(list.Take(list.Count - 1)).ToList();
+        }
+    }
+}
